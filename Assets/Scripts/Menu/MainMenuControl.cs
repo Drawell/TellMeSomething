56cb0df7f800779a28fa3c4e36f6cc3cd6289b0a
Drawell <ivanevgnikonov@gmail.com>
2021-06-11 00:00:42 +0300

    Main menu, new tale menu

---
+++ Assets/Scripts/Menu/MainMenuControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour
+{
+    public void OnNewTaleClick()
+    {
+
+    }
+
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
